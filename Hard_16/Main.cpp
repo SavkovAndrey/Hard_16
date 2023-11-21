9f2b81eb5f018bafd38588a8bc8a480f7a19147f
SavkovAndrey <141626360+SavkovAndrey@users.noreply.github.com>
2023-11-21 21:00:51 +0300

    финал

--- Hard_16/Main.cpp
+++ Hard_16/Main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <iomanip>
+#include <vector>
 
 using namespace std;
+
+void sort(vector<int>& A);                                         // сортировка
 
 int main()
 {
@@ -10,7 +13,7 @@ int main()
 	
 	int C[6][8] {};
 
-	for (int i = 0; i < 6; i++)
+	for (int i = 0; i < 6; i++)                       // Заполняем и выводим
 	{
 		for (int j = 0; j < 8; j++)
 		{
@@ -21,6 +24,44 @@ int main()
 	}
 	cout << endl;
 
+	vector<int> A;
+
+	for (int i = 0; i < 6; i++)                        // Заполнение вектора А положительными элементами из массива С
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			if (C[i][j] > 0)
+			{
+				A.push_back(C[i][j]);
+			}
+		}
+	}
+
+	sort(A);
+
+	for (int i = 0; i < A.size(); i++)                 // вывод вектора А
+	{
+		cout << setw(4) << A[i];
+	}
+	cout << endl;
+
 	system("pause");
 	return 0;
+}
+
+void sort(vector<int>&A)
+{
+	int temp = 0;;
+	for (int i = 0; i < A.size(); i++)
+	{
+		for (int j = 0; j < A.size() - 1; j++)
+		{
+			if (A[j] > A[j + 1])
+			{
+				temp = A[j];
+				A[j] = A[j + 1];
+				A[j + 1] = temp;
+			}
+		}
+	}
 }
